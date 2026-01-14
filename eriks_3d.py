9da1b85d5d65701114b6d099837c1d5536e863c3
Johan Thelin <e8johan@gmail.com>
2026-01-14 16:02:17 +0100

    Lägg till en andra triangel och centrera origo

--- eriks_3d.py
+++ eriks_3d.py
@@ -11,18 +11,26 @@ class Object:
         return []
 
 class Blupp(Object):
-    def __init__(self):
-        pass
+    def __init__(self, z):
+        self.z = z
 
     def vertexes(self):
-        return [Vector3(100, 100, 100), Vector3(100, 500, 100), Vector3(400, 300, 100)]
+        return [Vector3(-100, -100, self.z), Vector3(100, -500, self.z), Vector3(400, 300, self.z)]
 
 
-def render_objects(screen, objects):
+def projection(d, screen_center, v):
+    return (screen_center[0] + v.x*d/(v.z+d), screen_center[1] + v.y*d/(v.z+d))
+
+def render_objects(screen, d, objects):
+    w = screen.get_width()
+    h = screen.get_height()
+
+    screen_center = (w/2, h/2)
+
     for o in objects:
         vs = o.vertexes()
         for i in range(len(vs)):
-            pygame.draw.line(screen, (255, 255, 255), (vs[i].x, vs[i].y), (vs[(i+1) % len(vs)].x, vs[(i+1) % len(vs)].y))
+            pygame.draw.line(screen, (255, 255, 255), projection(d, screen_center, vs[i]), projection(d, screen_center, vs[(i+1) % len(vs)]))
 
 
 pygame.init()
@@ -30,7 +38,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-objects = [Blupp()]
+objects = [Blupp(100), Blupp(200)]
 
 while running:
     # poll for events
@@ -42,7 +50,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    render_objects(screen, objects)
+    render_objects(screen, 640, objects)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
