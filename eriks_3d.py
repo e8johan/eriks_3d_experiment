7fd99da78833b2ff0fff9c306d58b77d34f650b6
Johan Thelin <e8johan@gmail.com>
2026-01-14 15:55:17 +0100

    Rita en första triangel

--- eriks_3d.py
+++ eriks_3d.py
@@ -1,9 +1,36 @@
 import pygame
 
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Object:
+    def vertexes(self):
+        return []
+
+class Blupp(Object):
+    def __init__(self):
+        pass
+
+    def vertexes(self):
+        return [Vector3(100, 100, 100), Vector3(100, 500, 100), Vector3(400, 300, 100)]
+
+
+def render_objects(screen, objects):
+    for o in objects:
+        vs = o.vertexes()
+        for i in range(len(vs)):
+            pygame.draw.line(screen, (255, 255, 255), (vs[i].x, vs[i].y), (vs[(i+1) % len(vs)].x, vs[(i+1) % len(vs)].y))
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
+objects = [Blupp()]
 
 while running:
     # poll for events
@@ -15,7 +42,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    # RENDER YOUR GAME HERE
+    render_objects(screen, objects)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
